bca3d819481b51df57b64fe171f0ceba810fdda7
Auto User <mprov@jhu.edu>
2024-12-19 12:59:23 -0500

    push the changes back to main.

--- update-slurm-packages.sh
+++ update-slurm-packages.sh
@@ -61,10 +61,9 @@ tar -jcf slurm-${VERSION}${RELEASE}.tar.bz2 slurm-${VERSION}${RELEASE}
 
 rm -rf slurm-${VERSION}${RELEASE}
 
-git add . 
 git config --global user.name "Auto User"
 git config --global user.email "mprov@jhu.edu"
 
 git commit -am "Automated build of slurm $TAG"
-git push 
+git push origin main
 
